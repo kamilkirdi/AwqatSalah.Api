39c49ba356198a3fe96d2410c4f5c65f4d505c3b
Kamil KIRDI <kamilkirdi@msn.com>
2023-05-09 18:01:08 +0300

    Cache Controller Adding...

---
+++ DiyanetNamazVakti.Api/Controllers/CacheController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiyanetNamazVakti.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CacheController : ControllerBase
+{
+    private readonly ICacheService _cacheService;
+
+    public CacheController(ICacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+
+    [HttpGet("ClearCache")]
+    //[ApiExplorerSettings(IgnoreApi = true)]
+    public ActionResult<IResult> ClearCache()
+    {
+        _cacheService.Clear();
+        return new SuccessDataResult<bool>(true);
+    }
+
+    [HttpGet("GetCacheNames")]
+    //[ApiExplorerSettings(IgnoreApi = true)]
+    public ActionResult<IResult> GetCacheName()
+    {
+        return new SuccessDataResult<List<string>>(_cacheService.GetAllKeys());
+    }
+}
